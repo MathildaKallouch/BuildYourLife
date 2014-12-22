fde3813d8427369f188d826adec42d5956972601
Kratorg <romain.simon.vivier@gmail.com>
2014-12-22 13:06:54 +0100

    intégration des sons

--- BuildYourLife/Assets/Scripts/ClickableObject.cs
+++ BuildYourLife/Assets/Scripts/ClickableObject.cs
@@ -3,9 +3,25 @@ using System.Collections;
 
 public abstract class ClickableObject : MonoBehaviour 
 {
-	
+    private AudioSource m_source;
+
+    public AudioClip _audioClip;
+
+    void Start()
+    {
+        m_source = GameObject.Find("SoundEffects").GetComponent<AudioSource>();
+    }
+
 	public virtual void OnClick()
     {
+
+        if (m_source != null && _audioClip != null)
+        {
+            m_source.Stop();
+            m_source.clip = _audioClip;
+            m_source.Play();
+        }
+
         Manager.Instance.IsBlank();
     }
 }
--- BuildYourLife/Assets/Scripts/SkinTone.cs
+++ BuildYourLife/Assets/Scripts/SkinTone.cs
@@ -20,7 +20,6 @@ public class SkinTone : ClickableObject
         }
 
 
-
         base.OnClick();
     }
 
