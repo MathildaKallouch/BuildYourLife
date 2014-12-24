49c8aa07ebf1c97ca2183334aaa389763472db50
Kratorg <romain.simon.vivier@gmail.com>
2014-12-24 11:47:46 +0100

    mise à jour

---
+++ BuildYourLife/Assets/Scripts/RendererSortingOrder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RendererSortingOrder : MonoBehaviour
+{
+    public int _order;
+
+    // Use this for initialization
+    void OnValidate()
+    {
+        renderer.sortingLayerID = 0;
+        renderer.sortingOrder = _order;
+    }
+}
